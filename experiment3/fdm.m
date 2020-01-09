1706ae435ca09038f32832a167a18f288efed083
TA <namasikanam@gmail.com>
2020-01-10 00:43:34 +0800

    Finish

--- experiment3/fdm.m
+++ experiment3/fdm.m
@@ -37,10 +37,14 @@ ylabel('Magnitude');
 
 % encode
 dft = zeros(1, N);
-dft(1: N / 4) = dft_LetItGo(1: N / 4);
-dft(N / 4 + 1: N / 2) = dft_LetItGo(N * 3 / 4 + 1: N);
-dft(N / 2 + 1: N * 3 / 4) = dft_BlowinInTheWind(1: N / 4);
-dft(N * 3 / 4 + 1: N) = dft_BlowinInTheWind(N * 3 / 4 + 1: N);
+% dft(1: N / 4) = dft_LetItGo(1: N / 4);
+% dft(N / 4 + 1: N / 2) = dft_LetItGo(N * 3 / 4 + 1: N);
+% dft(N / 2 + 1: N * 3 / 4) = dft_BlowinInTheWind(1: N / 4);
+% dft(N * 3 / 4 + 1: N) = dft_BlowinInTheWind(N * 3 / 4 + 1: N);
+dft(1: N / 8) = dft_LetItGo(1: N / 8);
+dft(N / 4 + 1: N * 3 / 8) = dft_LetItGo(N * 7 / 8 + 1: N);
+dft(N / 2 + 1: N * 5 / 8) = dft_BlowinInTheWind(1: N / 8);
+dft(N * 3 / 4 + 1: N * 7 / 8) = dft_BlowinInTheWind(N * 7 / 8 + 1: N);
 
 % Plot the encode result in frequency domain
 figure('Name', 'Encoding result in Frequency Domain');
@@ -49,7 +53,6 @@ xlabel('frequency (Hz)');
 ylabel('Magnitude');
 
 % Plot the encoding result in time domain
-dft_ = dft;
 audio = ifft(dft);
 audiowrite('mixed.wav', abs(audio) / max(abs(audio)), fs);
 
@@ -59,36 +62,38 @@ xlabel('t (s)');
 ylabel('Magnitude');
 
 % decode
-dft = fft(audio);
-dft_LetItGo = [dft(1: N / 4) zeros(1, N / 2) dft(N / 4 + 1: N / 2)];
-dft_BlowinInTheWind = [dft(N / 2 + 1: N * 3 / 4) zeros(1, N / 2) dft(N * 3 / 4 + 1: N)];
+decoded_dft = fft(audio);
+% dft_LetItGo = [dft(1: N / 4) zeros(1, N / 2) dft(N / 4 + 1: N / 2)];
+% dft_BlowinInTheWind = [dft(N / 2 + 1: N * 3 / 4) zeros(1, N / 2) dft(N * 3 / 4 + 1: N)];
+decoded_dft_LetItGo = [decoded_dft(1: N / 8) zeros(1, N * 3 / 4) decoded_dft(N / 4 + 1: N * 3 / 8)];
+decoded_dft_BlowinInTheWind = [decoded_dft(N / 2 + 1: N * 5 / 8) zeros(1, N * 3 / 4) decoded_dft(N * 3 / 4 + 1: N * 7 / 8)];
 
 % Plot in frequency domain
 figure('Name', 'Decoded Let It Go in Frequency Domain');
-plot(frequencies, abs(dft_LetItGo), '.b');
+plot(frequencies, abs(decoded_dft_LetItGo), '.b');
 xlabel('frequency (Hz)');
 ylabel('Magnitude');
 
 figure('Name', 'Decoded Blowin''In The Wind in Frequency Domain');
-plot(frequencies, abs(dft_LetItGo), '.k');
+plot(frequencies, abs(decoded_dft_LetItGo), '.k');
 xlabel('frequency (Hz)');
 ylabel('Magnitude');
 
 % IFFT
-audio_LetItGo = abs(ifft(dft_LetItGo));
-audio_BlowinInTheWind = abs(ifft(dft_BlowinInTheWind));
+decoded_audio_LetItGo = abs(ifft(decoded_dft_LetItGo));
+decoded_audio_BlowinInTheWind = abs(ifft(decoded_dft_BlowinInTheWind));
 
 % Plot in time domain
 figure('Name', 'Decoded Let It Go in Time Domain');
-plot(times, audio_LetItGo, '.b');
+plot(times, decoded_audio_LetItGo, '.b');
 xlabel('t (s)');
 ylabel('Magnitude');
 
 figure('Name', 'Decoded Blowin''In The Wind in Time Domain');
-plot(times, audio_LetItGo, '.k');
+plot(times, decoded_audio_LetItGo, '.k');
 xlabel('t (s)');
 ylabel('Magnitude');
 
 % Write
-audiowrite('decodedLetItGo.wav', audio_LetItGo / max(abs(audio_LetItGo)), fs);
-audiowrite('decodedBlowinInTheWind.wav', audio_BlowinInTheWind / max(abs(audio_BlowinInTheWind)), fs);
+audiowrite('decodedLetItGo.wav', decoded_audio_LetItGo / max(abs(decoded_audio_LetItGo)), fs);
+audiowrite('decodedBlowinInTheWind.wav', decoded_audio_BlowinInTheWind / max(abs(decoded_audio_BlowinInTheWind)), fs);
